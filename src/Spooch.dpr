ab2ab885ad9907678abaf79b429056a10cc00762
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-16 14:43:17 +0100

    upgraded to Delphi 12.2 Athens, updated libraries path for Olf.RTL.Params, added Skia4Delphi to the project (#42), enabled Metal library for macOS (and iOS) (#42)

--- src/Spooch.dpr
+++ src/Spooch.dpr
@@ -1,13 +1,13 @@
 program Spooch;
 
 uses
-  System.StartUpCopy,
   FMX.Forms,
+  FMX.Types,
+  FMX.Skia,
   fMain in 'fMain.pas' {frmMain},
   uBruitages in 'uBruitages.pas',
   uConfig in 'uConfig.pas',
   uMusic in 'uMusic.pas',
-  Olf.RTL.Params in '..\lib-externes\librairies\Olf.RTL.Params.pas',
   cImgSpaceBackground in 'cImgSpaceBackground.pas' {cadImgSpaceBackground: TFrame},
   JoystickManager in 'JoystickManager.pas',
   Gamolf.FMX.Joystick in '..\lib-externes\FMXGameEngine\src\Gamolf.FMX.Joystick.pas',
@@ -17,11 +17,17 @@ uses
   Gamolf.RTL.Joystick in '..\lib-externes\FMXGameEngine\src\Gamolf.RTL.Joystick.pas',
   Gamolf.RTL.Scores in '..\lib-externes\FMXGameEngine\src\Gamolf.RTL.Scores.pas',
   iOSapi.GameController in '..\lib-externes\FMXGameEngine\src\iOSapi.GameController.pas',
-  Macapi.GameController in '..\lib-externes\FMXGameEngine\src\Macapi.GameController.pas';
+  Macapi.GameController in '..\lib-externes\FMXGameEngine\src\Macapi.GameController.pas',
+  Olf.RTL.Params in '..\lib-externes\librairies\src\Olf.RTL.Params.pas';
 
 {$R *.res}
 
 begin
+  GlobalUseSkia := True;
+  GlobalUseSkiaRasterWhenAvailable:=false;
+  {$IFDEF MACOS}
+  globalusemetal:=true;
+  {$ENDIF}
   Application.Initialize;
   Application.FormFactor.Orientations := [TFormOrientation.Landscape, TFormOrientation.InvertedLandscape];
   Application.CreateForm(TfrmMain, frmMain);
