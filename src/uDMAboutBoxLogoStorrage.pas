f8bbc535523da5f61909e10f2147aef771b0777b
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-16 16:33:20 +0100

    added the About Dialog box logo

---
+++ src/uDMAboutBoxLogoStorrage.pas
@@ -0,0 +1,59 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Spooch
+///
+/// Copyright 2021-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// A game developed as a FireMonkey project in Delphi.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://spooch.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Spooch
+///
+/// ***************************************************************************
+/// File last update : 2025-02-16T15:56:16.000+01:00
+/// Signature : 6e33fee54536047f86e1ddce399e4de1e6014b31
+/// ***************************************************************************
+/// </summary>
+
+unit uDMAboutBoxLogoStorrage;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Classes,
+  System.ImageList,
+  FMX.ImgList;
+
+type
+  TdmAboutBoxLogo = class(TDataModule)
+    imgLogo: TImageList;
+  private
+  public
+  end;
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+{$R *.dfm}
+
+end.
