723be33c1f5bd60d3ac10ba040960b79d34f104c
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-16 16:33:35 +0100

    added default background scene from the starter kit

---
+++ src/uSceneBackground.pas
@@ -0,0 +1,85 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Gamolf FMX Game Starter Kit
+///
+/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// The "Gamolf FMX Game Starter Kit" is both a "technical" example of a video
+/// game developed in Delphi with everything you need inside and a reusable
+/// project template you can customize for your own games.
+///
+/// The files provided are fully functional. Numerous comments are included in
+/// the sources to explain how they work and what you need to copy, override
+/// or customize to make video games without starting from scratch.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://fmxgamestarterkit.developpeur-pascal.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Gamolf-FMX-Game-Starter-Kit
+///
+/// ***************************************************************************
+/// File last update : 2024-08-10T09:04:46.000+02:00
+/// Signature : f0c3c44ee60074de2614de2f6cf724647eefbd89
+/// ***************************************************************************
+/// </summary>
+
+unit uSceneBackground;
+
+interface
+
+{$MESSAGE WARN 'If this scene interest you save this file in your project folder and customize the copy. Don''t change the template version if you want to be able to update it.'}
+// TODO : If this scene interest you save this file in your project folder and customize the copy. Don''t change the template version if you want to be able to update it.
+// TODO : If you don't want it in your project remove the unit from your project
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Graphics,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Dialogs,
+  FMX.StdCtrls,
+  _ScenesAncestor,
+  FMX.Objects;
+
+type
+  TSceneBackground = class(T__SceneAncestor)
+    Rectangle1: TRectangle;
+  private
+  protected
+  public
+    procedure ShowScene; override;
+  end;
+
+implementation
+
+{$R *.fmx}
+
+procedure TSceneBackground.ShowScene;
+begin
+  inherited;
+  SendToBack;
+end;
+
+end.
