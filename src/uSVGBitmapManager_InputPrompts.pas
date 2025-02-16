9be66903e424d535bfa31e586bcc42903a208b95
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-16 20:38:46 +0100

    added a unit to get bitmaps from SVG

---
+++ src/uSVGBitmapManager_InputPrompts.pas
@@ -0,0 +1,84 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Spooch
+///
+/// Copyright 2021-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// A game developed as a FireMonkey project in Delphi.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://spooch.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Spooch
+///
+/// ***************************************************************************
+/// File last update : 2025-02-16T19:04:12.000+01:00
+/// Signature : e825021a0dfa08d0e61cd228eec06551154cd3a7
+/// ***************************************************************************
+/// </summary>
+
+unit uSVGBitmapManager_InputPrompts;
+
+interface
+
+uses
+  FMX.Graphics,
+  USVGInputPrompts,
+  USVGKenneyShips;
+
+/// <summary>
+/// Returns a bitmap from a SVG image
+/// </summary>
+function getBitmapFromSVG(const Index: TSVGInputPromptsIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+
+function getBitmapFromSVG(const Index: TSVGKenneyShipsIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+
+implementation
+
+uses
+  Olf.Skia.SVGToBitmap;
+
+function getBitmapFromSVG(const Index: TSVGInputPromptsIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+begin
+  result := TOlfSVGBitmapList.Bitmap(ord(Index) + TSVGInputPrompts.Tag,
+    round(width), round(height), BitmapScale);
+end;
+
+function getBitmapFromSVG(const Index: TSVGKenneyShipsIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+begin
+  result := TOlfSVGBitmapList.Bitmap(ord(Index) + TSVGKenneyShips.Tag,
+    round(width), round(height), BitmapScale);
+end;
+
+procedure RegisterSVGImages;
+begin
+  TSVGInputPrompts.Tag := TOlfSVGBitmapList.AddItem(SVGInputPrompts);
+  TSVGKenneyShips.Tag := TOlfSVGBitmapList.AddItem(SVGKenneyShips);
+end;
+
+initialization
+
+RegisterSVGImages;
+
+end.
