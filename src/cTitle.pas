01431baf06dce640e2599868d428e421bf5d4202
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-04-28 09:03:36 +0200

    updated copyright, timestamp and signature comments

--- src/cTitle.pas
+++ src/cTitle.pas
@@ -1,3 +1,39 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Spooch
+///
+/// Copyright 2021-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// A game developed as a FireMonkey project in Delphi.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://spooch.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Spooch
+///
+/// ***************************************************************************
+/// File last update : 2025-04-27T15:25:48.000+02:00
+/// Signature : ecbdd6dbfa81796978443ebf6bd301c68905e66f
+/// ***************************************************************************
+/// </summary>
+
 unit cTitle;
 
 interface
--- src/uSpoochGameData.pas
+++ src/uSpoochGameData.pas
@@ -1,3 +1,39 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Spooch
+///
+/// Copyright 2021-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// A game developed as a FireMonkey project in Delphi.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://spooch.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Spooch
+///
+/// ***************************************************************************
+/// File last update : 2025-04-27T19:33:22.000+02:00
+/// Signature : a0c270d73f418d3c7c11532bb06470b6a8210a43
+/// ***************************************************************************
+/// </summary>
+
 unit uSpoochGameData;
 
 interface
